ca92cc501e0127bcc9d4729838d7eb5fb9fbe9b5
asigba <alexsigba@gmail.com>
2025-06-01 20:36:59 -0400

    added new shapes and sounds

--- ShapeGame/Assets/Scripts/Voicelines.cs
+++ ShapeGame/Assets/Scripts/Voicelines.cs
@@ -15,11 +15,25 @@ public class Voicelines : MonoBehaviour
         if (voicelines != null && voicelines.Length > 0)
         {
             int idx = Random.Range(1, voicelines.Length);
+            // Popping sound
             audioSource.PlayOneShot(voicelines[0]);
             if (voicelines.Length > 1)
             {
-                yield return new WaitForSeconds(1f);
-                audioSource.PlayOneShot(voicelines[idx]);
+                if (voicelines.Length == 3)
+                {
+                    // Color of shape
+                    audioSource.PlayOneShot(voicelines[1]);
+                    yield return new WaitForSeconds(0.5f);
+
+                    // Shape
+                    audioSource.PlayOneShot(voicelines[2]);
+                }
+                else
+                {
+                    //Random compliments
+                    yield return new WaitForSeconds(1f);
+                    audioSource.PlayOneShot(voicelines[idx]);
+                }
             }
         }
     }
